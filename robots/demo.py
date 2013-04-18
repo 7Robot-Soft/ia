151272ead3d85715e2ba52dfd52f91522e58119c
Élie Bouttier <elie.bouttier@free.fr>
2013-04-18 23:34:20 +0200

    Add demo robot

---
+++ robots/demo.py
@@ -0,0 +1,22 @@
+from robots.robot import Robot
+
+class DemoRobot(Robot):
+    
+    def __init__(self):
+        
+        super().__init__()
+
+        # Zone de départ : red|blue
+        self.side = "red"
+
+        # Dimension du robot
+        self.dimensions = { "left": 1290, "right": 1290,
+                "front": 1190, "back": 920 }
+
+        # Position initial du robot
+        self.pos = [-12000, -7000]
+        # Orientation initial du robot
+        self.rot = 0
+
+        # Périphérique
+        self.devices = { "asserv": 1305, "sensor": 1302 }
